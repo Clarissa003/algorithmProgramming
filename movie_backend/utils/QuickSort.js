ab8670f491f66fa60ca572b3f28499a8720d43bf
Alicia-Rodriguez1 <111961581+Alicia-Rodriguez1@users.noreply.github.com>
2025-04-08 21:16:24 +0200

    Edit: added comments

--- movie_backend/utils/QuickSort.js
+++ movie_backend/utils/QuickSort.js
@@ -1,10 +1,13 @@
 export function quickSort(arr, compareFn) {
+    //if only 1 element, no need for sorting
     if (arr.length <= 1) return arr;
 
-    const pivot = arr[0];
+    const pivot = arr[0]; //first element
+    //subarrays
     const left = [];
     const right = [];
 
+    //start from index 1 to skip the pivot
     for (let i = 1; i < arr.length; i++) {
         if (compareFn(arr[i], pivot) < 0) {
             left.push(arr[i]);
